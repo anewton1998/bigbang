54a03f1aeb931fd1f313848c870e01f901c5dd98
Andy Newton <andy@hxr.us>
2019-07-19 09:19:02 -0400

    new cli tests

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -14,14 +14,40 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+use std::io::*;
+use std::process::Command;
+
+use assert_cmd::prelude::*;
+use tempfile::Builder;
+use std::path::{Path, PathBuf};
+
 mod test_utils;
 
 #[test]
 fn test_test() {
+
     let d = test_utils::project_path();
     eprintln!("project directory {}",d.display());
 
     let d2 = test_utils::project_test_resources();
     eprintln!("test resources in {}", d2.display());
     assert_eq!(4,4);
+}
+
+#[test]
+fn new_directory_test() -> Result<()> {
+    let d = Builder::new().prefix("new_directory_test").tempdir()?;
+
+    let mut cmd = Command::cargo_bin("bigbang").unwrap();
+    cmd.arg("-d")
+        .arg(d.path())
+        .arg("new-directory");
+    let status = cmd.status().unwrap();
+    assert!(status.success());
+
+    let mut p = PathBuf::from( d.path() );
+    p.push("bigbang.toml");
+    assert!(p.is_file());
+
+    Ok(())
 }
